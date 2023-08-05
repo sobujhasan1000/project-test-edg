ec11682dad0c0f86a1fed5e01efc3df799bce1be
simanto97 <saimumsimanto97@gmail.com>
2023-08-05 13:15:22 +0600

    card added

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Carousel from "../Carousel/Carousel";
+import Partners from "../Partners/Partners";
 
 const Home = () => {
   return (
     <div>
       <Carousel />
+      <Partners />
     </div>
   );
 };
